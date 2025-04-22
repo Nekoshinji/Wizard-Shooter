627568bf5fa15fb29d7b7c08bed0f471a0d633d1
Nekoshinji <obito26@gmail.com>
2025-04-22 12:45:43 +0200

    first commit

---
+++ main.lua
@@ -0,0 +1,75 @@
+-- Débogueur Visual Studio Code tomblind.local-lua-debugger-vscode
+if pcall(require, 'lldebugger') then
+  require('lldebugger').start()
+end
+
+-- Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
+io.stdout:setvbuf('no')
+
+SCREEN_SIZE = {width = 800, height = 600}
+
+local wizard = {}
+wizard.move = {}
+wizard.position = {x = SCREEN_SIZE.width * .5, y = SCREEN_SIZE.height * .5}
+wizard.speed = 100
+wizard.direction = 0
+wizard.angle = 0
+wizardCurrentMove = 1
+table.insert(wizard.move, love.graphics.newImage('assets/images/Idle1.png'))
+table.insert(wizard.move, love.graphics.newImage('assets/images/Idle2.png'))
+table.insert(wizard.move, love.graphics.newImage('assets/images/Idle3.png'))
+table.insert(wizard.move, love.graphics.newImage('assets/images/Idle4.png'))
+table.insert(wizard.move, love.graphics.newImage('assets/images/Idle5.png'))
+table.insert(wizard.move, love.graphics.newImage('assets/images/Idle6.png'))
+table.insert(wizard.move, love.graphics.newImage('assets/images/Idle7.png'))
+table.insert(wizard.move, love.graphics.newImage('assets/images/Idle8.png'))
+table.insert(wizard.move, love.graphics.newImage('assets/images/Idle9.png'))
+local offset = {x = wizard.move[wizardCurrentMove]:getWidth() * .5, y = wizard.move[wizardCurrentMove]:getHeight() * .5}
+
+local powerCircleImg = love.graphics.newImage('assets/images/Big Circle.png')
+local fireBallPosition = {x = 0, y = 0}
+local fireBallAngle = 0
+local fireBallDistance = 50
+
+function love.load()
+end
+
+function love.update(dt)
+  if love.keyboard.isDown('d') then
+    wizard.position.x = wizard.position.x + wizard.speed * dt
+  elseif love.keyboard.isDown('q') then
+    wizard.position.x = wizard.position.x - wizard.speed * dt
+  elseif love.keyboard.isDown('s') then
+    wizard.position.y = wizard.position.y + wizard.speed * dt
+  elseif love.keyboard.isDown('z') then
+    wizard.position.y = wizard.position.y - wizard.speed * dt
+  end
+  wizardCurrentMove = wizardCurrentMove + 10 * dt
+  if wizardCurrentMove > 9 then
+    wizardCurrentMove = 1
+  end
+
+  -- if not (direction == 0) then
+  --   local dx = math.cos(wizard.angle) * wizard.speed * wizard.direction * dt
+  --   local dy = math.sin(wizard.angle) * wizard.speed * wizard.direction * dt
+  --   wizard.position.x = wizard.position.x + dx
+  --   wizard.position.y = wizard.position.y + dy
+  -- end
+
+  local mouseX, mouseY = love.mouse.getPosition()
+  local dx = mouseX - wizard.position.x
+  local dy = mouseY - wizard.position.y
+
+  fireBallAngle = math.atan2(dy, dx)
+
+  fireBallPosition.x = math.cos(fireBallAngle) * fireBallDistance + wizard.position.x
+  fireBallPosition.y = math.sin(fireBallAngle) * fireBallDistance + wizard.position.y
+end
+
+function love.draw()
+  love.graphics.draw(wizard.move[math.floor(wizardCurrentMove)], wizard.position.x, wizard.position.y, wizard.angle, 1, 1, offset.x, offset.y)
+  love.graphics.draw(powerCircleImg, fireBallPosition.x, fireBallPosition.y, fireBallAngle, 1, 1)
+end
+
+function love.keypressed(key)
+end
